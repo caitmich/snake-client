681725da371e537f972e25a40c9a2a96a52cccd6
Caitlin Henry <caitlinhenry.ss@gmail.com>
2021-08-25 14:51:22 -0400

    make input file to handle listener events

--- play.js
+++ play.js
@@ -1,27 +1,8 @@
 // const net = require("net");
+const {setupInput} = require('./input');
 const {connect} = require('./client');
 
 console.log("Connecting ...");
 connect();
-
-
-// setup interface to handle user input from stdin:
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-  
-};
 
 setupInput();
